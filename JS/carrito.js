8873034b13cd2d931416e11cb0607f9ab4607772
mariananizetich <mariananizetich@gmail.com>
2022-08-11 14:17:59 -0300

    terminando la entrega

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,3 +1,7 @@
+
+
+//carrito
+
 const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
 addToShoppingCartButtons.forEach((addToCartButton) => {
   addToCartButton.addEventListener('click', addToCartClicked);
@@ -39,6 +43,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
       updateShoppingCartTotal();
       return;
     }
+    
   }
 
   const shoppingCartRow = document.createElement('div');
@@ -79,6 +84,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
 }
 
 function updateShoppingCartTotal(e) {
+  
   let total = 0;
   const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
 
@@ -104,33 +110,203 @@ function updateShoppingCartTotal(e) {
 }
 
 function removeShoppingCartItem(event) {
+  
   const buttonClicked = event.target;
   buttonClicked.closest('.shoppingCartItem').remove();
   updateShoppingCartTotal();
-  guardarEnLocalStorage()
 }
 
 
 function quantityChanged(event) {
+  
   const input = event.target;
   input.value <= 0 ? (input.value = 1) : null;
   updateShoppingCartTotal();
-  guardarEnLocalStorage()
 }
 
 function comprarButtonClicked() {
   shoppingCartItemsContainer.innerHTML = '';
   updateShoppingCartTotal();
-  guardarEnLocalStorage()
+
 }
 
-
-function guardarCarritoEnLocalStorage () {
-  localStorage.setItem('carrito', JSON.stringify(shoppingCartContent));
-}
-
-function cargarCarritoDeLocalStorage () {
+/*function cargarCarritoDeLocalStorage () {
+  
   if (localStorage.getItem('carrito') !== null) {
-      carrito = JSON.parse(localStorage.getItem('shoppingCartContent'));
+     
+      carrito = JSON.parse(localStorage.getItem('carrito'));
   }
+}*/
+
+
+
+/*document.addEventListener('DOMContentLoaded', () => {
+
+  let carrito = [];
+  const DOMitems = document.getElementById('items');
+  const DOMcarrito = document.querySelector('#carrito');
+  const DOMtotal = document.querySelector('#total');
+  const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+  const DOMbotoncomprar = document.querySelector("#boton-comprar")
+
+
+  const listaDeProductos = [
+      {
+          id: 1,
+          nombre: "Avistaje",
+          precio: 10500,
+          capacidad: 10
+      },
+      {
+          id: 2,
+          nombre: "Snorkel",
+          precio: 9000,
+          capacidad: 10
+      },  
+      
+  ]
+  function renderizarProductos() {
+      listaDeProductos.forEach((info) => {
+          // Estructura
+          const miNodo = document.createElement('div');
+          miNodo.classList.add('item', 'col-sm-12', "col-md-6", "shadow", "d-flex", "mb-4", "col-12");
+          // Body
+          const miNodoCardBody = document.createElement('div');
+          miNodoCardBody.classList.add('card-body');
+          // Titulo
+          const miNodoTitle = document.createElement('h5');
+          miNodoTitle.classList.add('card-title');
+          miNodoTitle.innerText = info.nombre;
+          // Precio
+          const miNodoPrecio = document.createElement('p');
+          miNodoPrecio.classList.add('card-text');
+          miNodoPrecio.innerText = `$${info.precio}`;
+          //Stock
+          const miNodoStock = document.createElement('p');
+          miNodoStock.classList.add('card-text');
+          miNodoStock.innerText = `Capacidad: ${info.capacidad}`;
+          //Imagen
+          const miNodoImagen = document.createElement('img')
+          miNodoImagen.classList.add('imagen')
+          miNodoImagen.setAttribute('src', info.img)
+
+          // Boton 
+          const miNodoBoton = document.createElement('button');
+          miNodoBoton.classList.add('btn', "btn-light");
+          miNodoBoton.innerText = '+';
+          miNodoBoton.setAttribute('marcador', info.id);
+          miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+          // Insertamos
+          miNodoCardBody.append(miNodoTitle);
+          miNodoCardBody.append(miNodoPrecio);
+          miNodoCardBody.append(miNodoStock);
+          //miNodoCardBody.append(miNodoImagen)
+          miNodoCardBody.append(miNodoBoton);
+          miNodo.append(miNodoCardBody);
+          DOMitems.append(miNodo);
+      });
+  }
+
+   function anyadirProductoAlCarrito(e) {
+      carrito.push(e.target.getAttribute('marcador'))
+      renderizarCarrito();
+      guardarCarritoEnLocalStorage();
+  }
+
+  
+  function renderizarCarrito() {
+      // Vaciamos todo el html
+      DOMcarrito.innerText = '';
+      // Quitamos los duplicados
+      const carritoSinDuplicados = [...new Set(carrito)];
+      // Generamos los Nodos a partir de carrito
+      carritoSinDuplicados.forEach((item) => {
+          // Obtenemos el item que necesitamos de la variable base de datos
+          const miItem = listaDeProductos.filter((itemBaseDatos) => {
+              // ¿Coincide las id? Solo puede existir un caso
+              return itemBaseDatos.id === parseInt(item);
+          });
+          // Cuenta el número de veces que se repite el producto
+          const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+              // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+              return itemId === item ? total += 1 : total;
+          }, 0);
+          // Creamos el nodo del item del carrito
+          const miNodo = document.createElement('div');
+          miNodo.classList.add('align-items-center', 'text-right', 'mx-2');
+          miNodo.innerText = `${numeroUnidadesItem} x ${miItem[0].nombre} - $${miItem[0].precio}`;
+          // Boton de borrar
+          const miBoton = document.createElement('button');
+          miBoton.classList.add('btn', 'btn-light', 'mx-5');
+          miBoton.innerText = 'Vaciar';
+          miBoton.style.marginLeft = '1rem';
+          miBoton.dataset.item = item;
+          miBoton.addEventListener('click', borrarItemCarrito);
+          // Mezclamos nodos
+          miNodo.appendChild(miBoton);
+          DOMcarrito.appendChild(miNodo);
+      });
+      // Renderizamos el precio total en el HTML
+      DOMtotal.innerText = calcularTotal();
+  }
+
+  
+  function borrarItemCarrito(e) {
+      // Obtenemos el producto ID que hay en el boton pulsado
+      const id = e.target.dataset.item;
+      // Borramos todos los productos
+      carrito = carrito.filter((carritoId) => {
+          return carritoId !== id;
+      });
+      // volvemos a renderizar
+      renderizarCarrito();
+      // Actualizamos el LocalStorage
+      guardarCarritoEnLocalStorage();
+
+  }
+
+  
+  function calcularTotal() {
+      // Recorremos el array del carrito 
+      return carrito.reduce((total, item) => {
+          // De cada elemento obtenemos su precio
+          const miItem = listaDeProductos.filter((itemBaseDatos) => {
+              return itemBaseDatos.id === parseInt(item);
+          });
+          // Los sumamos al total
+          return total + miItem[0].precio;
+      }, 0).toFixed(2);
+  }
+
 }
+  function vaciarCarrito() {
+      // Limpiamos los productos guardados
+      carrito = [];
+      // Renderizamos los cambios
+      renderizarCarrito();
+      // Borra LocalStorage
+      localStorage.removeItem('carrito');
+
+  }
+
+  function guardarCarritoEnLocalStorage () {
+      localStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+
+  function cargarCarritoDeLocalStorage () {
+      // ¿Existe un carrito previo guardado en LocalStorage?
+      if (localStorage.getItem('carrito') !== null) {
+          // Carga la información
+          carrito = JSON.parse(localStorage.getItem('carrito'));
+      }
+  }
+
+  // Eventos
+  DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+  // Inicio
+  cargarCarritoDeLocalStorage();
+  renderizarProductos();
+  renderizarCarrito();
+});*/
+
--- JS/javascript.js
+++ JS/javascript.js
@@ -3,8 +3,8 @@ let email = document.getElementById("mail")
 formulario.addEventListener("submit", validarEnvio)
 
 
-
 function validarEnvio(e){
+    e.preventDefault ()
     if (email.value ==0){
         e.preventDefault ();
         document.getElementById("resultado").innerHTML = "El campo Email es obligatorio."
@@ -14,9 +14,12 @@ function validarEnvio(e){
         document.getElementById("resultado").innerHTML = "El campo Consulta es obligatorio."
     }
     else {
-    
+        e.preventDefault ()
       document.getElementById("resultado").innerHTML = "Tu consulta ha sido enviada con éxito. En breve nos pondemos en contacto con vos."
 
 }
         
 }
+ 
+
+
