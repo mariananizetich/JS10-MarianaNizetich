ecf1247e8a3b11457969fe13d59b31669fead466
mariananizetich <mariananizetich@gmail.com>
2022-08-17 13:54:33 -0300

    agregando librerías

--- JS/javascript.js
+++ JS/javascript.js
@@ -4,16 +4,28 @@
 
 let formulario = document.getElementById("formulario")
 let email = document.getElementById("mail")
+let consulta = document.getElementById("consulta")
+
 
 
 function validarEnvio(e){
     e.preventDefault ();
-    email.value == 0 || consulta.value == "" ? 
-    document.getElementById("resultado").innerHTML =  "Los campos Email y Consulta son obligatorios." : 
-    document.getElementById("resultado").innerHTML = "Tu consulta ha sido enviada con éxito."
+    (email.value == 0 || consulta.value == "") ? 
+    (Swal.fire({
+            icon: 'error',
+            title: '¡Ups!',
+            text: 'Los campos Email y Consulta son obligatorios.',}) ) 
+            :
+            (Swal.fire({
+            icon: 'success',
+            title: '¡Listo!',
+            text: 'Tu consulta fue enviada con éxito. En breve nos ponemos en contacto con vos.',})
+            )
+      }
 
 
-}
+
+
 
 
 formulario.addEventListener("submit", validarEnvio)
